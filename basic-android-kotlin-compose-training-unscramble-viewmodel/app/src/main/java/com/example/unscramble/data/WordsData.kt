47b8dda5146c714fee695ec04f63993c892d898c
erlinaputrirahmadhani05 <erlinarahmadhani05@gmail.com>
2024-09-13 14:36:17 +0700

    tugas-ke-1

---
+++ basic-android-kotlin-compose-training-unscramble-viewmodel/app/src/main/java/com/example/unscramble/data/WordsData.kt
@@ -0,0 +1,29 @@
+package com.example.unscramble.data
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// Set with all the words for the Game
+val allWords: Set<String> =
+    setOf(
+        "manggis",
+        "semangka",
+        "jeruk",
+        "durian",
+        "mangga",
+        "jambu",
+        "melon",
+        "pepaya",
+        "pisang",
+        "salak",
+        "alpukat",
+        "rambutan",
+        "anggur",
+        "strawberry",
+        "duku",
+        "sawo",
+        "kiwi",
+        "leci",
+        "cerry",
+        "bluberry"
+    )
